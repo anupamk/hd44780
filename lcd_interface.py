536152e16ce4a67e4bfe1e51d21c4e62d1ed9bdc
Anupam Kapoor <anupam.kapoor@gmail.com>
2010-03-15 03:51:47 +0900

    fixed some subtle errors during display. output is always wrapped...

--- lcd_interface.py
+++ lcd_interface.py
@@ -2,64 +2,82 @@
 import hd44780_driver as lcd_driver
 import hd44780_custom_chars as lcd_cc
 
-# display related imports
-from hd44780_driver import write_string_at
-from hd44780_driver import write_custom_character_at
-
 # lcd dimensions
 LCD_NUM_ROWS               = 4
 LCD_NUM_COLS               = 20
 LCD_MAX_DISPLAYABLE_STRING = LCD_NUM_COLS * LCD_NUM_ROWS
 
-# printf like interface for displaying stuff on to the
-# lcd. custom-characters are not supported...
-def printf(row, col, wrap_ok = True, fmt_args = None, *fmt_argv):
-    # basic checks first
+# this function returns the maximum number of displayable-characters
+# in the current-row. both rows and columns start from '1'.
+def get_max_displayable_chars_in_row(col_offset = 1):
+    max_chars = LCD_NUM_COLS
+
+    return max_chars - (col_offset - 1)
+
+# display a given string, returns the number of bytes actually written
+def display_string(row, col, disp_str):
+    num_chars_to_show = min(get_max_displayable_chars_in_row(col), len(disp_str))
+    lcd_row_str = disp_str[:num_chars_to_show]
+
+    lcd_driver.write_string_at(row, col, lcd_row_str)
+
+    return len(lcd_row_str)
+
+# this function returns 'True' if the arguments to lcd-printf are bad,
+# and 'False' otherwise
+def lcd_printf_bad_args(row, col, fmt_args):
     if ((fmt_args == None) or
         (row < 1 or row > LCD_NUM_ROWS) or                      
         (col < 1 or col > LCD_NUM_COLS)):
-        return
+        return True
 
-    # can we even fit the string in the available space ?
-    blank_cols = col + (row-1) * LCD_NUM_COLS
+    # overflowing display limits ?
+    blank_cols = (col-1) + (row-1) * LCD_NUM_COLS
     if blank_cols >= LCD_MAX_DISPLAYABLE_STRING:             
-        return                                       # nope
+        return True                                             # yes
+
+    return False
+
+# can dumping continue ? call epa...
+def continue_dumping(idx, lcd_output):
+    if ((idx >= len(lcd_output)) or
+        (idx >= LCD_MAX_DISPLAYABLE_STRING)):
+        return False
+
+    return True
+
+# 
+# this function implements a 'printf' like interface for displaying
+# stuff on the lcd. it returns the number of characters actually
+# dumped.
+#
+# first-row, first-col is defined as (1,1)
+def lcd_printf(row, col, fmt_args, *fmt_argv):
+    str_idx  = 0
     
-    # all-that-we-can-show VS what-we-actually-can-show
-    temp_lcd_output = str(fmt_args) % fmt_argv
-    lcd_output      = temp_lcd_output[:(LCD_MAX_DISPLAYABLE_STRING - blank_cols + 1)]
+    # bad arguments.
+    if lcd_printf_bad_args(row, col, fmt_args) == True:
+        return str_idx
+
+    lcd_output = str(fmt_args) % fmt_argv               # what-to-show
+
+    # dump the first row
+    nc      = display_string(row, col, lcd_output[str_idx:])
+    str_idx = nc
+
+    if continue_dumping(str_idx, lcd_output) == False:
+        return str_idx
     
-    # start dumping strings...
-    write_string_at(row, col, lcd_output[:LCD_NUM_COLS - col + 1])
+    # and the subsequent rows...
+    for i in range(row+1, LCD_NUM_ROWS+1):
+        nc = display_string(i, 1, lcd_output[str_idx:])
 
-    # truncated dump
-    if wrap_ok == False: return
+        # update
+        str_idx  = str_idx + nc
+        disp_col = 1
 
-    # wrap the whole thing around...
-    i = row + 1
-    j = LCD_NUM_COLS - col
-    while i <= LCD_NUM_ROWS:
-        write_string_at(i, 1, lcd_output[j:LCD_NUM_COLS + 1])
-        
-        i = i + 1
-        j = j + LCD_NUM_COLS
+        if continue_dumping(str_idx, lcd_output) == False:
+            break
 
-# show the shape. nothing fancy at all...
-def cc_printf(row, col, shape_name = None):
-    cc_cgram_loc = lcd_cc.get_custom_char_cgram_location(shape_name)
-
-    # can't do much
-    if cc_cgram_loc == None: return
-    
-    write_custom_character_at(row, col, cc_cgram_loc)
-
-
-# initialize the display.
-#    - load all the custom-characters
-#    - basic initialization
-def initialize():
-    lcd_cc.load_custom_chars_to_cgram()
-    lcd_driver.initialize()
-
-def reset():
-    lcd_driver.reset_lcd()
+    return str_idx
+     
