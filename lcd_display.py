c0af9d5e534caf8f82a223f97c98ba2b8bf12771
Anupam Kapoor <anupam.kapoor@gmail.com>
2010-03-15 03:52:26 +0900

    better interface for custom characters

--- lcd_display.py
+++ lcd_display.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
-import time
+import hd44780_driver        as lcd_driver             # low-level stuff
+import lcd_interface         as lcd_ascii              # vanilla-ascii
+import lcd_custom_characters as lcd_cc                 # custom-characters
 
-# lcd specific
-import lcd_interface as lcd
 
-# display something:
-def display_something():
-    lcd.printf(1, 1, "%s ", "hello world")                 # canonical
-    lcd.cc_printf(2,1, 'BELL')
-    
-# initialize the lcd first
-lcd.initialize()
+# a rudimentary class
+class CustomCharacter:
+    def __init__(self, **kwds):
+        self.__dict__.update(kwds)
+
+# lets define some custom characters
+#   - cgram_location == -1 below implies a value that will be assigned
+#     to during creation of the shape
+POPULAR_CUSTOM_CHARACTERS = [
+    CustomCharacter(cgram_location = -1, byte_seq = [0x04, 0x0e, 0x0e, 0x0e, 0x1f, 0x00, 0x04, 0x00], name = 'BELL'),
+    CustomCharacter(cgram_location = -1, byte_seq = [0x00, 0x00, 0x1f, 0x1f, 0x1f, 0x1f, 0x00, 0x00], name = 'BAR' )
+]
 
 
 
