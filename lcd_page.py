af2672482eb9296101184716d0c5a428f42eb8c6
Anupam Kapoor <anupam.kapoor@gmail.com>
2011-06-30 12:53:45 +0530

    reduced default "total_duration" to a smaller value. starts to look pretty boring.

--- lcd_page.py
+++ lcd_page.py
@@ -3,7 +3,7 @@
 # of custom shapes which can provide non-ascii display e.g. a cpu-usage meter
 # etc. 
 class lcd_page(object):
-    def __init__(self, disp_lcd, name, display_func, total_duration = 60.0, refresh_rate = 1.0):
+    def __init__(self, disp_lcd, name, display_func, total_duration = 30.0, refresh_rate = 1.0):
         self.lcd_obj       = disp_lcd
         self.name          = name
         self.pg_disp_time  = total_duration
